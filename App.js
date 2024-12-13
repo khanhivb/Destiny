de1eeb3a65e467e521e7ff7152d2a42a9ea9c07a
khanhivb <lytuong99aa@gmail.com>
2024-12-13 15:17:21 +0700

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 
 const App = () => {
-  const demo = '';
+  const demo = 'xxxxx';
   return <AppNavigator />;
 };
 
